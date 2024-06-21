f6fb6968ba5fcc7966ee3aa34b659f3f8f48e53e
brofu <brofu.talk@gmail.com>
2024-06-21 11:10:40 +0800

    add scripts to install node.js 10.24.1, npm 6 and gitbook 3.2.3

--- gitbook_installation.sh
+++ gitbook_installation.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-npm install -g gitbook-cli 
-gitbook install 
+# gitbook depends node.js and npm
+# GitBook 3.2.3 is compatible with node.js 10 and npm 6
+
+# install nvm to install old version of node.js (10.24.1)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.nvm/nvm.sh
+
+# install node 10
+nvm install 10
+nvm use 10
+
+# install npm 6
+npm install -g npm@6
+
+# install gitbook
+npm install -g gitbook-cli
+# install gitbook-summary
 npm install -g gitbook-summary 
                                 
 
